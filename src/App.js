7f8a85f3767a29ef4026c93c44786570e99a1fe7
Chigozie Oduah <oduahchigozie46@gmail.com>
2022-04-10 11:34:38 +0100

    implemeted form feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { useState } from "react";
+import { ethers } from "ethers";
 
 function App() {
+  let [text, setText] = useState('');
+
   return (
     <div className="App">
       <button onClick={() => {
@@ -7,10 +11,30 @@ function App() {
         if (ethereum) {
           ethereum.request({ method: 'eth_requestAccounts'})
             .then(accounts => {
-              console.log("Accounts connected\n", accounts);
+              setConnected(true);
             })
         }
       }}>Connect wallet</button>
+
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        let { ethereum } = window;
+        if (ethereum) {
+          let abi = JSON.parse('[{"inputs": [{"internalType": "string","name": "newText","type": "string"}],"name": "changeText","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [],"stateMutability": "nonpayable","type": "constructor"},{"inputs": [],"name": "text","outputs": [{"internalType": "string","name": "","type": "string"}],"stateMutability": "view","type": "function"}]')
+          let address = '0xf4B2e66AdE7e9B7794EBD7F3A306e1bE9De1f7A3';
+          let provider = new ethers.providers.Web3Provider(ethereum);
+          let signer = provider.getSigner();
+          let contract = new ethers.Contract(address, abi, signer);
+          contract.changeText(text)
+            .then(() => {
+              setText("");
+            });
+        }
+
+      }}>
+        <input type="text" placeholder="Enter text" onChange={e => setText(e.currentTarget.value)} value={text} />
+        <input type="submit" value="save to contract" />
+      </form>
     </div>
   );
 }
