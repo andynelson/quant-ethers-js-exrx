591d7ced4bde84d0af902c12943c1c664730addc
Chigozie Oduah <oduahchigozie46@gmail.com>
2022-04-11 00:05:08 +0100

    built the text retriever

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ethers } from "ethers";
 
 function App() {
   let [text, setText] = useState('');
+  const [savedText, setSavedText] = useState("");
   let [connected, setConnected] = useState(false);
 
   return (
@@ -22,20 +23,38 @@ function App() {
         let { ethereum } = window;
         if (ethereum) {
           let abi = JSON.parse('[{"inputs": [{"internalType": "string","name": "newText","type": "string"}],"name": "changeText","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [],"stateMutability": "nonpayable","type": "constructor"},{"inputs": [],"name": "text","outputs": [{"internalType": "string","name": "","type": "string"}],"stateMutability": "view","type": "function"}]')
-          let address = '0xf4B2e66AdE7e9B7794EBD7F3A306e1bE9De1f7A3';
+          let address = '0x354253459DE646F37b3BBdBAe7Ab26830F09feE6';
           let provider = new ethers.providers.Web3Provider(ethereum);
           let signer = provider.getSigner();
           let contract = new ethers.Contract(address, abi, signer);
           contract.changeText(text)
-            .then(() => {
-              setText("");
-            });
+          .then(() => {
+            setText("");
+          });
         }
-
+        
       }}>
         <input type="text" placeholder="Enter text" onChange={e => setText(e.currentTarget.value)} value={text} />
         <input type="submit" value="save to contract" />
       </form>
+
+      <button onClick={() => {
+        let { ethereum } = window;
+        if (ethereum) {
+          setSavedText('fetching...');
+          let abi = JSON.parse('[{"inputs": [{"internalType": "string","name": "newText","type": "string"}],"name": "changeText","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [],"stateMutability": "nonpayable","type": "constructor"},{"inputs": [],"name": "text","outputs": [{"internalType": "string","name": "","type": "string"}],"stateMutability": "view","type": "function"}]')
+          let address = '0x354253459DE646F37b3BBdBAe7Ab26830F09feE6';
+          let provider = new ethers.providers.Web3Provider(ethereum);
+          let signer = provider.getSigner();
+          let contract = new ethers.Contract(address, abi, signer);
+          contract.text()
+            .then(text => {
+              setSavedText(text);
+            })
+        }
+      }}>Get Text</button>
+
+      {savedText}
     </div>
   );
 }
