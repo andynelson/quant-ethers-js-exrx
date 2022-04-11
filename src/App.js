e6761731e0a49cc03bfc0a86a7df945dfe7d9362
Chigozie Oduah <oduahchigozie46@gmail.com>
2022-04-11 22:57:26 +0100

    refactored App.js

--- src/App.js
+++ src/App.js
@@ -1,62 +1,61 @@
 import { useState } from "react";
 import { ethers } from "ethers";
-
+ 
 function App() {
-  let [text, setText] = useState('');
-  const [savedText, setSavedText] = useState("");
+  let [text, setText] = useState("");
+  let [savedText, setSavedText] = useState("");
   let [connected, setConnected] = useState(false);
-
+ 
+  let { ethereum } = window;
+  let contract = null;
+ 
+  if (ethereum) {
+ 
+    let abi = JSON.parse('[{"inputs": [{"internalType": "string","name": "newText","type": "string"}],"name": "changeText","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [],"stateMutability": "nonpayable","type": "constructor"},{"inputs": [],"name": "text","outputs": [{"internalType": "string","name": "","type": "string"}],"stateMutability": "view","type": "function"}]')
+ 
+    let address = "0x2c0d9cc3e4D4eC872d782D7D530D30308AF41ed3";
+    let provider = new ethers.providers.Web3Provider(ethereum);
+    let signer = provider.getSigner();
+    contract = new ethers.Contract(address, abi, signer);
+  }
+ 
   return (
     <div className="App">
+ 
       <button onClick={() => {
-        let { ethereum } = window;
-        if (ethereum && !connected) {
-          ethereum.request({ method: 'eth_requestAccounts'})
-            .then(accounts => {
-              setConnected(true);
-            })
-        }
+          if (contract && connected) {
+              ethereum.request({ method: 'eth_requestAccounts'})
+                  .then(accounts => {
+                      setConnected(true);
+                  })
+          }
       }}>{!connected ? 'Connect wallet' : 'Connected' }</button>
-
+ 
       <form onSubmit={(e) => {
         e.preventDefault();
-        let { ethereum } = window;
-        if (ethereum) {
-          let abi = JSON.parse('[{"inputs": [{"internalType": "string","name": "newText","type": "string"}],"name": "changeText","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [],"stateMutability": "nonpayable","type": "constructor"},{"inputs": [],"name": "text","outputs": [{"internalType": "string","name": "","type": "string"}],"stateMutability": "view","type": "function"}]')
-          let address = '0x354253459DE646F37b3BBdBAe7Ab26830F09feE6';
-          let provider = new ethers.providers.Web3Provider(ethereum);
-          let signer = provider.getSigner();
-          let contract = new ethers.Contract(address, abi, signer);
+        if (contract && connected) {
           contract.changeText(text)
-          .then(() => {
-            setText("");
-          });
+            .then(() => {
+              setText("");
+            });
         }
-        
       }}>
-        <input type="text" placeholder="Enter text" onChange={e => setText(e.currentTarget.value)} value={text} />
-        <input type="submit" value="save to contract" />
+          <input type="text" placeholder="Enter text" onChange={e => setText(e.currentTarget.value)} value={text} />
+          <input type="submit" value="save to contract" />
       </form>
-
+ 
       <button onClick={() => {
-        let { ethereum } = window;
-        if (ethereum) {
-          setSavedText('fetching...');
-          let abi = JSON.parse('[{"inputs": [{"internalType": "string","name": "newText","type": "string"}],"name": "changeText","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [],"stateMutability": "nonpayable","type": "constructor"},{"inputs": [],"name": "text","outputs": [{"internalType": "string","name": "","type": "string"}],"stateMutability": "view","type": "function"}]')
-          let address = '0x354253459DE646F37b3BBdBAe7Ab26830F09feE6';
-          let provider = new ethers.providers.Web3Provider(ethereum);
-          let signer = provider.getSigner();
-          let contract = new ethers.Contract(address, abi, signer);
+        if (contract && connected) {
           contract.text()
             .then(text => {
               setSavedText(text);
             })
         }
       }}>Get Text</button>
-
-      {savedText}
+ 
+      <span>{savedText}</span>
     </div>
   );
 }
-
+ 
 export default App;
